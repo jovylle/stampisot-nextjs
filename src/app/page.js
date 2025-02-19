5b13bb8ab876859e227d76765097480a80348735
j <git@jovylle.com>
2025-02-19 17:22:53 +0800

    update A to Link

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-// src/app/page.js
+import Link from 'next/link';
 import UserList from '../components/UserList';
 
 export default function Home () {
@@ -6,8 +6,12 @@ export default function Home () {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         {/* ... existing code ... */}
-        <a href="/api/swagger" className="text-blue-600 underline">API Documentation (api/swagger)</a>
-        <a href="/redoc" className="text-blue-600 underline">API Documentation (redoc)</a>
+        <Link href="/api/swagger">
+          <a className="text-blue-600 underline">API Documentation (api/swagger)</a>
+        </Link>
+        <Link href="/redoc">
+          <a className="text-blue-600 underline">API Documentation (redoc)</a>
+        </Link>
         <UserList />
         {/* ... existing code ... */}
       </main>
