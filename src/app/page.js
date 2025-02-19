a04a5d46fe17e3333fcca7841670430cef880c21
j <git@jovylle.com>
2025-02-19 17:37:37 +0800

    remove A from index

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,11 @@ export default function Home () {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         {/* ... existing code ... */}
-        <Link href="/api/swagger">
-          <a className="text-blue-600 underline">API Documentation (api/swagger)</a>
+        <Link href="/api/swagger" className="text-blue-600 underline">
+          API Documentation (api/swagger)
         </Link>
-        <Link href="/redoc">
-          <a className="text-blue-600 underline">API Documentation (redoc)</a>
+        <Link href="/redoc" className="text-blue-600 underline">
+          API Documentation (redoc)
         </Link>
         <UserList />
         {/* ... existing code ... */}
