c210c253abe4a75b920a5bc1d1019e9d6721f097
j <git@jovylle.com>
2025-02-20 16:09:08 +0800

    fix tailwind setup

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,11 @@ import UserList from '../components/UserList';
 
 export default function Home () {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="text-center">
+      <main className="">
         {/* ... existing code ... */}
         <Link href="/api/swagger" className="text-blue-600 underline">
-          API Documentation (api/swagger)
-        </Link>
-        <Link href="/redoc" className="text-blue-600 underline">
-          API Documentation (redoc)
+          API Documentation (docs)
         </Link>
         <UserList />
         {/* ... existing code ... */}
